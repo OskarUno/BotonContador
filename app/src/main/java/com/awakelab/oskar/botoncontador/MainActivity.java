ac8f4f623c083ecc22c0a7c1149464f2a64ff7bb
OskarUno <oorellana80@gmail.com>
2023-06-19 22:10:52 -0400

    Se agrego funcionalidad solicitada

--- app/src/main/java/com/awakelab/oskar/botoncontador/MainActivity.java
+++ app/src/main/java/com/awakelab/oskar/botoncontador/MainActivity.java
@@ -4,11 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initListeners();
+    }
+
+    private void initListeners() {
+        Button btnToast = findViewById(R.id.btnToast);
+        Button btnCounter = findViewById(R.id.btnCount);
+        TextView texNumero = findViewById(R.id.texNumero);
+
+        btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Hello Toast!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnCounter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer numAdd = Integer.parseInt(texNumero.getText().toString());
+                numAdd++;
+                texNumero.setText(numAdd.toString());
+            }
+        });
     }
 }
